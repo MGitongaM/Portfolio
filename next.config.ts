6f4cc35a719ebcf598de815175c8f31fbc02de0d
Michael Gitonga <gitoshmbae@gmail.com>
2025-11-05 15:50:35 +0300

    Update isProd to is Production next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
 
   output: "export", // Required for static export
-  basePath: isProd ? "/Portfolio" : "",
+  basePath: isProduction ? "/Portfolio" : "",
   images: {
     unoptimized: true, // Required for static export
   },
