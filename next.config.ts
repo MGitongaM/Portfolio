91194c8b47c8cf5c571e98e161a983fdf71fbc76
Michael Gitonga <gitoshmbae@gmail.com>
2025-11-05 16:36:43 +0300

    Utilizes the use of next cloudinary to serve the images and turning the components where CldImage is used to client componets, Reverts to the default config  in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
 
-  output: "export", // Required for static export
-  basePath: isProduction ? "/Portfolio" : "",
-  assetPrefix: isProduction ? '/Portfolio/' : '',
-  images: {
-    unoptimized: true, // Required for static export
-  },
+  // output: "export", // Required for static export
+  // basePath: isProduction ? "/Portfolio" : "",
+  // assetPrefix: isProduction ? '/Portfolio/' : '',
+  // images: {
+  //   unoptimized: true, // Required for static export
+  // },
 };
 
 export default nextConfig;
