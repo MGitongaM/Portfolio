e648bb93a50d0f1e471b050c0d5b7d4a36358397
Michael Gitonga <gitoshmbae@gmail.com>
2025-11-05 15:25:39 +0300

    Adds more config options to enable the images to be shown in production in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === 'production';
-
+const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
 
-  
-  basePath: isProduction ? '/Portfolio' : '',
-  assetPrefix: isProduction ? '/Portfolio' : '',
- 
+  output: "export", // Required for static export
+  basePath: isProduction ? "/Portfolio" : "",
+  assetPrefix: isProduction ? "/Portfolio" : "",
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
